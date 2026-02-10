473763034218e42c7e2c41b08ad2c747e42a416c
builtbysuraj <surajgupta3553@gmail.com>
2026-02-10 07:55:20 +0530

    deploy: Generate fresh requirements.txt and fix server running main function

--- main.py
+++ main.py
@@ -44,4 +44,6 @@ app.include_router(authentication_router)
 app.include_router(notes_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT")))
+    # Cloud Run provides PORT as an env var; default to 8080 if not found
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
