c65c51c58e1dd4d26473c38621202088f9fa4015
Ashok <ashokmommyguy@gmail.com>
2026-03-05 11:47:34 +0530

    converted int to string for all id fields

--- src/controllers/deals.py
+++ src/controllers/deals.py
@@ -10,6 +10,25 @@ def get_deals(page,db:Session, user_id:int):
         offset = (page - 1) * limit
         total_records = db.query(Deal).filter(Deal.deal_owner_id == user_id).count()
         deals = db.query(Deal).filter(Deal.deal_owner_id == user_id).offset(offset).limit(limit).all()
+        for deal in deals:
+
+                if deal.id:
+                    deal.id = str(deal.id)
+
+                if deal.deal_owner_id:
+                    deal.deal_owner_id = str(deal.deal_owner_id)
+
+                if deal.assignee_id:
+                    deal.assignee_id = str(deal.assignee_id)
+
+                if deal.account_id:
+                    deal.account_id = str(deal.account_id)
+
+                if deal.created_by:
+                    deal.created_by = str(deal.created_by)
+
+                if deal.modified_by:
+                    deal.modified_by = str(deal.modified_by)
         total_pages = math.ceil(total_records / limit)
         return {"data":deals,"page_info":{
             "total_pages":total_pages,
